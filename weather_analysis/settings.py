9693ddccdf14f976bb4bdcde05cb4f86f4d91c86
sikha <sikha6721@gmail.com>
2024-08-22 13:12:28 +0530

    Update port binding for Render deployment

--- weather_analysis/settings.py
+++ weather_analysis/settings.py
@@ -124,3 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PORT = os.getenv('PORT', '8000')
